87ae5d8e05b6adca8de55cf9e3c88a9cb32eafa5
lshanka <lav.shankar@gmail.com>
2021-10-03 12:38:45 +0100

    Update Class1.cs
    Updated a few comments

--- Class1.cs
+++ Class1.cs
@@ -7,16 +7,24 @@ public  class TalkToCatAPI
 	
 	public CatsInformation[] TalkToCat()
 	{
-		//method steps:
-		//talk to CATAPI webpage
-		//gets the response as json
-		//deseialise the json and store information as array of CatsInformation class
-		//returns (array of CatsInformation class) 
+		/*method steps:
+		talk to CATAPI webpage
+		gets the response as json
+		deseialise the json and store information as array of CatsInformation class
+		returns (array of CatsInformation class) */
+
+		//calling the CATAPI webpage through RestClient and sending a Get request
 		var client = new RestClient("https://api.thecatapi.com/v1/breeds");
 		var request = new RestRequest(Method.GET);
+
+		//adding my API key in the HTML header section
 		request.AddHeader("x-api-key", "8e14a33b-c328-43c0-8050-9517dec17f17");
+
+		//getting a response from the CATAPI
 		IRestResponse response = client.Execute(request);
 		var resultFromAPI=response.Content;
+
+		//deserialising the json content into array of type CatsInformation
 		var catArray = JsonConvert.DeserializeObject<CatsInformation[]>(resultFromAPI);
 		return (catArray);
 	}
