2099698126bfc0754951782b48c6e581c09f2ad8
lshanka <lav.shankar@gmail.com>
2021-10-03 12:30:37 +0100

    Final Version
    Comments added and all checked through

--- Class2.cs
+++ Class2.cs
@@ -2,7 +2,7 @@
 
 public class CatsInformation
 {
-	
+	//creating a class that contains properties of cats
 	public string Name { get; set; }
 	public string Id { get; set; }
 	public string Origin { get; set; }
--- Program.cs
+++ Program.cs
@@ -6,23 +6,20 @@ namespace CatAPITesting
     {
         static void Main(string[] args)
         {
+            //calling the TalkToCat method of TalkToCatAPI class to return an array of type CatsInformation
             TalkToCatAPI objTalkToCatAPI = new TalkToCatAPI();
-            var catInfo = new CatsInformation();
             var Cats = objTalkToCatAPI.TalkToCat();
 
+            //creating a header before ouput:
+            Console.WriteLine("CAT Name, Origin,       Id,           Intelligence");
 
-            Console.WriteLine("CAT Name, Origin", "Id", "Intelligence");
-            foreach(var cat in Cats)
+            //parsing and printing the CatsInformation array
+            foreach (var cat in Cats)
             {
-                //Console.WriteLine(cat.ToString());
-                Console.WriteLine($"Cat name: {catInfo.Name}, Origin: {catInfo.Origin}, ID:{catInfo.Id},Intelligence:{catInfo.Intelligence}");
+                Console.WriteLine($"Cat name: {cat.Name}, Origin: {cat.Origin}, ID:{cat.Id},Intelligence:{cat.Intelligence}");
             }
-
-            {
-
-
-                Console.ReadLine();
-            }
+            Console.ReadLine();
+            
         }
     }
 }
